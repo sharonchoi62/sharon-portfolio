3916cc6d06e3baba62a3f60082ad7c9742c84212
sharonchoi62 <sharonchoi62@gmail.com>
2018-08-09 13:45:45 -0700

    portfolio-responsive-design-1st-sub

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Sticky Nav Bar for #section-nav
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("section-menu");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
